4c528a88461d58c5a89ad21358690f8b1cbac052
rudraprasaaad <goudarudra23@gmail.com>
2025-05-18 20:57:15 +0530

    feat: added shadcn and dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import ModeToggle from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className="max-w-7xl mx-auto flex justify-between my-2">
+      Home page
+      <div className="flex gap-2">
+        <ModeToggle />
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button className="bg-indigo-500 hover:bg-indigo-400 px-4 py-2 rounded-md hover:cursor-pointer">
+              Sign in
+            </Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
 }
