9715a1a4e965ee416b68a802e8f614529406223b
rudraprasaaad <goudarudra23@gmail.com>
2025-05-19 05:48:06 +0530

    feat: synced auth and db

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import DesktopNavbar from "./desktop-navbar";
 import MobileNavbar from "./mobile-navbar";
-export default function Navbar() {
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
+export default async function Navbar() {
+  const user = await currentUser();
+  if (user) await syncUser();
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
