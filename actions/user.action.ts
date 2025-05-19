b7658c40c12a595c30a608be21b472926048b221
rudraprasaaad <goudarudra23@gmail.com>
2025-05-19 22:08:23 +0530

    feat: added like comment delete and create post feature

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -54,7 +54,7 @@ export async function getUserByClerkId(clerkId: string) {
 
 export async function getDbUserId() {
   const { userId: clerkId } = await auth();
-  if (!clerkId) throw new Error("Unauthorized");
+  if (!clerkId) return null;
 
   const user = await getUserByClerkId(clerkId);
 
@@ -66,6 +66,7 @@ export async function getDbUserId() {
 export async function getRandomUsers() {
   try {
     const userId = await getDbUserId();
+    if (!userId) return [];
     const randomUsers = await prisma.user.findMany({
       where: {
         AND: [
@@ -105,6 +106,8 @@ export async function getRandomUsers() {
 export async function toggleFollow(targetUserId: string) {
   try {
     const userId = await getDbUserId();
+
+    if (!userId) return;
 
     if (userId === targetUserId) throw new Error("You cannot follow yourself");
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
 import CreatePost from "@/components/create-post";
+import PostCard from "@/components/post-card";
 import WhoToFollow from "@/components/who-to-follow";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
   const user = await currentUser();
   const posts = await getPosts();
+  const dbUserId = await getDbUserId();
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
         {user ? <CreatePost /> : null}
         <div className="space-y-6">
-          {posts.map((post) => (
-            <PostCard key={post.id} post={post} />
+          {posts?.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
           ))}
         </div>
       </div>
