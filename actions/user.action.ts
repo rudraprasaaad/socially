be7e4048b1299d3e5c26ef68422d8938ec01a419
rudraprasaaad <goudarudra23@gmail.com>
2025-05-19 08:46:42 +0530

    feat: added create post

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -1,6 +1,7 @@
 "use server";
 import { prisma } from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
+
 export async function syncUser() {
   try {
     const { userId } = await auth();
@@ -31,4 +32,32 @@ export async function syncUser() {
   } catch (err) {
     console.log("Error in syncUser", err);
   }
+}
+
+export async function getUserByClerkId(clerkId: string) {
+  return prisma.user.findUnique({
+    where: {
+      clerkId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          following: true,
+          posts: true,
+        },
+      },
+    },
+  });
+}
+
+export async function getDbUserId() {
+  const { userId: clerkId } = await auth();
+  if (!clerkId) throw new Error("Unauthorized");
+
+  const user = await getUserByClerkId(clerkId);
+
+  if (!user) throw new Error("User not found");
+
+  return user.id;
 }
